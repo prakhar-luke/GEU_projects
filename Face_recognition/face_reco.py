a6f119573d79098249f6188a7b9caf4171d48359
Utkarsh Kushwaha <54776016+UtkarshKushwaha1@users.noreply.github.com>
2021-05-14 00:08:12 +0530

    Add files via upload

---
+++ Face_recognition/face_reco.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'Attendence Images'
+images = []
+classNames = []
+mylist = os.listdir(path)
+print(mylist)
+ 
+for cl in mylist:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodelist = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
+
+encodeListKnown = findEncodings(images)
+print('Encoding Completed!')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    sucess ,img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    faces_in_current_frame = face_recognition.face_locations(imgS)
+
+    encodes_current_frame = face_recognition.face_encodings(imgS,faces_in_current_frame)
+
+    for encodeFaces , faceloc in zip(encodes_current_frame,faces_in_current_frame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFaces)
+        facedis = face_recognition.face_distance(encodeListKnown,encodeFaces)
+        #print(facedis)
+
+        matchIndex =np.argmin(facedis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+
+            y1,x2,y2,x1 = faceloc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(39,154,157),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(39,154,157),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
